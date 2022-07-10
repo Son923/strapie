455c0daa1824522ff7a2c94aa077ec00e4e7bafe
Son <thaison1804@gmail.com>
2022-07-11 01:01:58 +0700

    aha

--- src/plugins/youtube-search/admin/src/components/LeftCol/index.js
+++ src/plugins/youtube-search/admin/src/components/LeftCol/index.js
@@ -8,21 +8,21 @@ import { Searchbar, SearchForm } from '@strapi/design-system/Searchbar';
 import { Button } from '@strapi/design-system/Button';
 
 const LeftCol = () => {
-    const ROW_COUNT = 8;
-    const COL_COUNT = 2;
+  const ROW_COUNT = 8;
+  const COL_COUNT = 2;
+	const API_KEY = 'AIzaSyCWqTrhgrGfB-WyUG_wANcdOjnO4Z8-YyM';
 	const [value, setValue] = useState('');
-	const [data, setData] = useState('');
 
 	const field = {
 		channelID : 'UCZW5lIUz93q_aZIkJPAC0IQ',
-		channelOwner : '88rising',
-		channelStartDate : '2016-03-18',
 		channelDescription : '88 is double happiness',
 		subscriberCount : '5830000',
 		videoCount : '938',
 		totalViews : '2831785624',
 		channelLink : 'https://www.youtube.com/channel/UCZW5lIUz93q_aZIkJPAC0IQ'
 	};
+	
+	const [data, setData] = useState(field);
 
 	function youtube_parser(url){
 		var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
@@ -32,9 +32,19 @@ const LeftCol = () => {
 
 	const handleSearchRequest = async () => {
 		const videoId = await youtube_parser(value);
-		const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=AIzaSyCWqTrhgrGfB-WyUG_wANcdOjnO4Z8-YyM`);
+		const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`);
 		const res = await response.json();
-		setData(res);
+		console.log(res.items[0].snippet);
+		const abc = {
+			channelID : res.items[0].snippet.channelId,
+			channelDescription : res.items[0].snippet.description,
+			subscriberCount : '5830000',
+			videoCount : '938',
+			totalViews : '2831785624',
+			channelLink : `https://www.youtube.com/channel/${res.items[0].snippet.channelId}`
+		}
+		setData(abc);
+		console.log(abc);
 	};
 
     return <Box padding={4}>
@@ -60,16 +70,7 @@ const LeftCol = () => {
 									<Typography variant="sigma">CHANNEL ID</Typography>
 								</Td>
 								<Td>
-									<Typography textColor="neutral800">{ data.items }</Typography>
-								</Td>
-							</Tr>
-
-							<Tr>
-								<Td>
-									<Typography variant="sigma">OWNER</Typography>
-								</Td>
-								<Td>
-									<Typography textColor="neutral800">{ field.channelOwner	}</Typography>
+									<Typography textColor="neutral800">{ data.channelID }</Typography>
 								</Td>
 							</Tr>
 
@@ -78,8 +79,8 @@ const LeftCol = () => {
 									<Typography variant="sigma">LINK</Typography>
 								</Td>
 								<Td>
-									<BaseLink href={ field.channelLink } isExternal>
-										<Typography textColor="neutral800">{ field.channelLink	}</Typography>
+									<BaseLink href={ data.channelLink } isExternal>
+										<Typography textColor="neutral800">{ data.channelLink	}</Typography>
 									</BaseLink>
 								</Td>	
 							</Tr>
@@ -89,16 +90,7 @@ const LeftCol = () => {
 									<Typography variant="sigma">DESCRIPTION</Typography>
 								</Td>
 								<Td>
-									<Typography textColor="neutral800">{field.channelDescription	}</Typography>
-								</Td>
-							</Tr>
-
-							<Tr>
-								<Td>
-									<Typography variant="sigma">CHANNEL START DATE</Typography>
-								</Td>
-								<Td>
-									<Typography textColor="neutral800">{ field.channelStartDate }</Typography>
+									<Typography textColor="neutral800">{data.channelDescription	}</Typography>
 								</Td>
 							</Tr>
 
@@ -117,14 +109,6 @@ const LeftCol = () => {
 								</Td>
 								<Td>
 									<Typography textColor="neutral800">{ field.totalViews }</Typography>
-								</Td>
-							</Tr>
-							<Tr>
-								<Td>
-									<Typography variant="sigma">TEST</Typography>
-								</Td>
-								<Td>
-									<Typography textColor="neutral800">{ value }</Typography>
 								</Td>
 							</Tr>
 
